ed77a9803979c4e1b51e1008167ab40081e6d21c
Carlo Emilio Montanari <carlidel95@gmail.com>
2022-04-02 20:55:51 +0200

    test

--- wsgi.py
+++ wsgi.py
@@ -267,4 +267,5 @@ def update_fig(*args):
 ###############################################################################
 
 if __name__ == '__main__':
+    print("Test")
     app.run_server(host="0.0.0.0", port=8080, debug=True)
