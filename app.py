072f16f1c7ee94ebeb5ad5279bf81c7f2e840bab
Carlo Emilio Montanari <carlidel95@gmail.com>
2022-04-02 19:37:10 +0200

    maybe this?

--- app.py
+++ app.py
@@ -276,4 +276,4 @@ def update_fig(*args):
 ###############################################################################
 
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=8080, debug=True)
+    app.run_server(port=8080, debug=True)
